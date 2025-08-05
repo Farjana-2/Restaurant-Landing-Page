988a6738ae787afa08e7c271c6bc4e591bea9987
Shaik Farjana <shaikfarjana0212@gmail.com>
2025-08-05 21:55:57 +0530

    Create script.js

---
+++ Restaurant Landing Page/script.js
@@ -0,0 +1,5 @@
+function confirmReservation() {
+  document.getElementById("reservation-status").innerText = "✅ Reservation Confirmed!";
+  document.getElementById("reservation-status").style.color = "green";
+  return false; 
+}
